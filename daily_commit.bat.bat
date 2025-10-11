43b1f6c01638b57e73c161e70a4aa376d783b208
Yeshwanth-45 <yesh6333@gmail.com>
2025-10-11 15:57:39 +0530

    Daily commit 1 - 11-10-2025 15:57:39.94

--- daily_commit.bat.bat
+++ daily_commit.bat.bat
@@ -1,26 +1,47 @@
 @echo off
-cd C:\Users\yesh9\github-daily
+cd /d "C:\Users\yesh9\github-daily"
 
-:: Set your correct Git identity
-git config user.name "Yeshwanth-45"
-git config user.email "yesh6333@gmail.com"
+echo ===========================================
+echo Starting 7 daily commits on %date% %time%
+echo ===========================================
 
-:: Generate 3 commits daily
-echo Commit 1 - %date% %time% >> daily_log.txt
+rem Commit 1
+echo Daily commit 1 - %date% %time% >> log1.txt
 git add .
 git commit -m "Daily commit 1 - %date% %time%"
-git push origin main
 
-timeout /t 3 >nul
-
-echo Commit 2 - %date% %time% >> daily_log.txt
+rem Commit 2
+echo Daily commit 2 - %date% %time% >> log2.txt
 git add .
 git commit -m "Daily commit 2 - %date% %time%"
-git push origin main
 
-timeout /t 3 >nul
-
-echo Commit 3 - %date% %time% >> daily_log.txt
+rem Commit 3
+echo Daily commit 3 - %date% %time% >> log3.txt
 git add .
 git commit -m "Daily commit 3 - %date% %time%"
+
+rem Commit 4
+echo Daily commit 4 - %date% %time% >> log4.txt
+git add .
+git commit -m "Daily commit 4 - %date% %time%"
+
+rem Commit 5
+echo Daily commit 5 - %date% %time% >> log5.txt
+git add .
+git commit -m "Daily commit 5 - %date% %time%"
+
+rem Commit 6
+echo Daily commit 6 - %date% %time% >> log6.txt
+git add .
+git commit -m "Daily commit 6 - %date% %time%"
+
+rem Commit 7
+echo Daily commit 7 - %date% %time% >> log7.txt
+git add .
+git commit -m "Daily commit 7 - %date% %time%"
+
 git push origin main
+
+echo ===========================================
+echo All 7 commits done successfully!
+pause
